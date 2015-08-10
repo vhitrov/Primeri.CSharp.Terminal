d9e7eab2ac9087f7aac9fced966cc3342443a800
Vasil Hitrov <vhitrov@hotmail.com>
2015-08-10 10:31:07 +0300

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Modul_02/Modul_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul_02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзола
+
+		}
+	}
+}
