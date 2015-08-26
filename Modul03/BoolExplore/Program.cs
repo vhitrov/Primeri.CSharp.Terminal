6c3f30b75a68cdc0c80c19eebfd16b7ea5e0f186
Vasil Hitrov <vhitrov@hotmail.com>
2015-08-26 20:21:13 +0300

    Модул 03, Видео 07 - Примери с тип bool - 2-ра част

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -10,9 +10,15 @@ namespace BoolExplore
 			//bool _test=5>10;
 
 			int[] _i = new int[] { 4, 3, 2 };
-			int _ui = Convert.ToInt32 (Console.ReadLine ());
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui > 0;
+			int _ui = 0; //Convert.ToInt32 (Console.ReadLine ());
+			string _input = "0";
+			Console.Write ("Моля въведете индекс: ");
+			_input = Console.ReadLine ();
+
+
+			bool _check01 = int.TryParse(_input, out _ui);
+			bool _check02 = _ui <= _i.Length; 
+			bool _check03 = _ui > 0;
 
 //			bool _exp1 = _check01 && _check02; //Логическо "И"(&&)
 //			bool _exp2 = _check01 || _check02; //Логическо "ИЛИ"(||)
@@ -23,18 +29,23 @@ namespace BoolExplore
 //			Console.WriteLine ("Логическо ИЛИ: " + _exp2.ToString());
 //			Console.WriteLine ("Отрицанието на " + _check01.ToString () + " е: " + _exp3);
 
-			if (_ui > 0 && _ui <= _i.Length) {
+			if (_check01 && _check02 && _check03) {
 				Console.WriteLine (_i [_ui - 1]);
-			} else {
-				if (_ui > _i.Length) 
-				{
-					Console.WriteLine ("Индекса е прекалено голям \n\n\n");
-				}
-				if (_ui <=  0) 
-				{
-					Console.WriteLine ("Индекса е прекалено малък \n\n\n");
-				}
 			}
+
+			if (!_check01) {
+				Console.WriteLine ("Индекса трябва да бъде цяло число!!! \n\n\n");
+			}
+
+			if (! _check02) {
+				Console.WriteLine ("Индекса е прекалено голям!!! \n\n\n");
+				}
+
+			if (!_check03 && _check01) {
+				Console.WriteLine ("Индекса е прекалено малък!!! \n\n\n");
+			}
+			
+
 		}
 
 			//Console.WriteLine (_i[_ui-1]);
