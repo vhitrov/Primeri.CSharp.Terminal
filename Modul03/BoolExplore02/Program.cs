756aa7bf82579c1a46f5083abdf8cbae80edd6a4
Vasil Hitrov <vhitrov@hotmail.com>
2015-08-31 18:57:11 +0300

    Модул 03, Видео 09 - Използване на оператора Switch

--- Modul03/BoolExplore02/Program.cs
+++ Modul03/BoolExplore02/Program.cs
@@ -12,12 +12,38 @@ namespace BoolExplore02
 			Console.Write ("Моля въведете индекс: ");
 			_input = Console.ReadLine ();
 			bool _check01 = int.TryParse(_input, out _ui);
-			_ui = (_ui<=_i.Length) ? _ui:3;
-			_ui = (_ui > 0) ? _ui : 1;
+//			_ui = (_ui<=_i.Length) ? _ui:3;
+//			_ui = (_ui > 0) ? _ui : 1;
 
-			if(_check01) {
-				Console.WriteLine (_i [_ui - 1]);
+//			if(_check01) {
+//				Console.WriteLine (_i [_ui - 1]);
+//						}
+
+			switch(_ui){
+			case 1:
+				{
+					Console.Write ("Първи елемент от масива: ");
+					Console.WriteLine(_i[_ui-1]);
+					break;
+				}
+			case 2:
+				{
+					Console.Write ("Втори елемент от масива: ");
+					Console.WriteLine(_i[_ui-1]);
+					break;
+				}
+			case 3:
+				{
+					Console.Write ("Трети елемент от масива: ");
+					Console.WriteLine(_i[_ui-1]);
+					break;
+				}
+			default:
+				{
+					Console.Write ("В масива няма стойност за този индекс!\nОпитайте с индекс м/у 1 и 3.\n\n ");
+					break;
+				}
+			}
 		}
-	}
 	}
 }
