b7426e7583c02ee66d1f710cd5325c91ec2c540b
Vasil Hitrov <vhitrov@hotmail.com>
2015-09-10 14:36:58 +0300

    Модул 04, Видео 05 - Цикъл do...while

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -27,17 +27,27 @@ namespace Modul04
 //					Console.Write (_table [i, j].PadRight (12));
 //
 //				}
-
-				int i=0;
-			while(i< _table.Length/3){
+			//Пример с While
+//			int i=0;
+//			while(i< _table.Length/3){
+//				Console.WriteLine (
+//					_table [i, 0].PadRight (12) +
+//					_table [i, 1].PadRight (12) +
+//					_table [i, 2].PadRight (12));
+//				i++;
+//					}
+//
+//				Console.WriteLine ();
+			//Примери с do...while
+			int i=0;
+			do {
+				//Код
 				Console.WriteLine (
 					_table [i, 0].PadRight (12) +
 					_table [i, 1].PadRight (12) +
 					_table [i, 2].PadRight (12));
 				i++;
-				}
-
-				Console.WriteLine ();
+			} while(i < _table.Length / 3);
 			}
 		}
 	}
