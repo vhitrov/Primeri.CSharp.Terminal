19170ecb94cc3ae79056fb9bc36d046bad8a5024
Vasil Hitrov <vhitrov@hotmail.com>
2015-09-21 09:17:36 +0300

    Модул 05, Видео 02 - Използване на референции в методите

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -7,32 +7,56 @@ namespace Modul05
 		public static void Main (string[] args)
 		{
 			//Дефиниции
-			string usercomand="";
+			string usercomand = "";
 
 			//Потрбителски команди
-			do 
-			{
-				Console.Write ("$ "); usercomand=Console.ReadLine ();
-				if(usercomand.Contains("cmd01")) cmd01 (usercomand);
-				if(usercomand.Contains("cmd02")) Console.WriteLine(cmd02(usercomand));
+			do {
+				Console.Write ("$ ");
+				usercomand = Console.ReadLine ();
+				if (usercomand.Contains ("cmd01"))
+					cmd01 (usercomand);
+				if (usercomand.Contains ("cmd02"))
+					Console.WriteLine (cmd02 (usercomand));
 
-			} while(usercomand !="exit");
+				if (usercomand.Contains ("cmd03")) {
+					double _a = 0;
+					if (cmd03 (usercomand, out _a)) {
+						Console.WriteLine ("a.a=" + _a.ToString ());
+					} else {
+						Console.WriteLine ("Командата не е въведена коректно!");
+					}
+				}
+				if (usercomand.Contains("cmd04"))
+				{
+					double _t=5, _c=-2;
+					if (cmd04(ref _t))
+					{
+						Console.WriteLine (_t);
+					}
+					if (cmd04(ref _c))
+					{
+						Console.WriteLine(0);
 
+					}
+				}
+
+			} while(usercomand != "exit");
 		}
+		
 		public static void cmd01 (string _input) //cmd01 5-->5^2=25
 		{
 			try
 			{
 				double _temp=0;
 				if(double.TryParse(_input.Split(' ')[1], out _temp)) {
-					Console.WriteLine("\nРезултат от"+_temp.ToString()+"^2"+(_temp*_temp).ToString());
+					Console.WriteLine("\nРезултат от "+_temp.ToString()+"^2 e "+(_temp*_temp).ToString());
 				}
 					
 			} catch{
 			}
 		}
 
-		public static int cmd02 (string _input)
+		public static int cmd02 (string _input) //cmd02 5-->5^3=125
 		{
 			int _temp = 0;
 			if (int.TryParse (_input.Split (' ') [1], out _temp)) {
@@ -40,5 +64,30 @@ namespace Modul05
 			}
 			return _temp;
 		}
+
+		public static bool cmd03 (string _input, out double _i)
+		{
+				try
+				{
+					string _p=_input.Split(' ')[1];
+					double __p=0;
+				if((double.TryParse(_p, out __p)))
+						{
+							_i=__p*__p;
+							return true;
+						}
+
+						}catch{
+						}
+						_i=0;
+						return false;
+		}
+		public static bool cmd04(ref double _i)
+		{
+			double _temp = _i;
+			_i = _i * _i;
+			return(_temp > 0);
+		}
 	}
 }
+						
